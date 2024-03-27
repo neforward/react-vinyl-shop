71a0e056edfe6f08643294a3128c4ddabb318d93
neforward <141821349+neforward@users.noreply.github.com>
2024-03-27 17:06:38 +0600

    commit lala

--- src/components/ArrowTop.jsx
+++ src/components/ArrowTop.jsx
@@ -32,7 +32,7 @@ const ArrowTop = () => {
                 onClick={scrollToTop}
                 title="Go to top"
             >
-                <img src="../../public/arrow.svg" alt="" />
+
             </button>
         </>
     )
